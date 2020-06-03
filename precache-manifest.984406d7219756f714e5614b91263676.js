15a367626792ad4949e2e08f5d4ef74a9aaa24a0
StefanMitrikeski <mi34ence94@gmail.com>
2020-06-03 15:21:26 +0200

    Updates

---
+++ precache-manifest.984406d7219756f714e5614b91263676.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1704687c14e184da4b27ac5243883112",
+    "url": "/StefanMitrikeski/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "4aa98862fe5509e1ef21",
+    "url": "/StefanMitrikeski/MonstersRolodex/static/css/main.779b3aa1.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/StefanMitrikeski/MonstersRolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/StefanMitrikeski/MonstersRolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aa98862fe5509e1ef21",
+    "url": "/StefanMitrikeski/MonstersRolodex/static/js/main.7bee194e.chunk.js"
+  },
+  {
+    "revision": "aa541aec2482ec56a77b",
+    "url": "/StefanMitrikeski/MonstersRolodex/static/js/runtime-main.6e9b9a11.js"
+  }
+]);
